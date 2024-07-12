f1e9949b6e076e732d98c49a2df8fcc4841d566a
Isaque <isaquebatista400@gmail.com>
2024-07-12 16:11:08 -0300

    closes #2, #8 - Criando metodo POST

--- backend/src/main/java/com/cadastro/backend/controller/CourseController.java
+++ backend/src/main/java/com/cadastro/backend/controller/CourseController.java
@@ -2,18 +2,16 @@ package com.cadastro.backend.controller;
 
 import java.util.Arrays;
 import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
 
 import com.cadastro.backend.models.Course;
 
 
 @RestController
+@CrossOrigin
 public class CourseController {
   private List<Course> courses = Arrays.asList(
       new Course(1, "Java"),
--- backend/src/main/java/com/cadastro/backend/controller/StudentController.java
+++ backend/src/main/java/com/cadastro/backend/controller/StudentController.java
@@ -1,23 +1,32 @@
 package com.cadastro.backend.controller;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cadastro.backend.models.Student;
 
 @RestController
+@CrossOrigin
 public class StudentController {
 
-  // private List<Student> students = new ArrayList<>();
-  private List<Student> students = Arrays.asList(
-      new Student(1, "Pedro Antonio", "pedro@gmail.com", "(11)96399-5039", 1, "Tarde"));
+  private List<Student> students = new ArrayList<>();
+  // private List<Student> students = Arrays.asList(
+  // new Student(1, "Pedro Antonio", "prdro@gmail.com", "(11)963995039", 1,
+  // "Tarde")
+  // );
 
   @GetMapping("students/{id}")
   public ResponseEntity<Student> getStudent(@PathVariable("id") int id) {
@@ -28,13 +37,23 @@ public class StudentController {
     return ResponseEntity.ok(stud);
   }
 
+  @PostMapping("students")
+  public ResponseEntity<Student> save(@RequestBody Student student) {
+    student.setId(students.size() + 1);
+    students.add(student);
+
+    URI location = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(student.getId())
+        .toUri();
+
+    return ResponseEntity.created(location).body(student);
+  }
+
   @GetMapping("students")
   public List<Student> getStudents() {
     return students;
   }
-
-  // private List<Student> students = Arrays.asList(
-  // new Student(1, "Glauco", "Todesco", "13311-215", "Centro", "Sorocaba", "SP")
-  // );
 
 }
