9ef1a34c0b9402e67bc7135311cbff7cfb82ab90
Isaque <isaquebatista400@gmail.com>
2024-07-12 15:08:24 -0300

    Criando endpoint students/id

--- backend/src/main/java/com/cadastro/backend/controller/StudentController.java
+++ backend/src/main/java/com/cadastro/backend/controller/StudentController.java
@@ -4,29 +4,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cadastro.backend.models.Student;
 
 @RestController
 public class StudentController {
-  
+
   // private List<Student> students = new ArrayList<>();
   private List<Student> students = Arrays.asList(
-    new Student(1, "Glauco", "Todesco", "Tua Leite", "13311-215", "Centro", "Sorocaba", "SP")
-  );
+      new Student(1, "Glauco", "Todesco", "Tua Leite", "13311-215", "Centro", "Sorocaba", "SP"));
 
-
-  
+  @GetMapping("students/{id}")
+  public ResponseEntity<Student> getStudent(@PathVariable("id") int id) {
+    Student stud = students.stream()
+        .filter(s -> s.getId() == id)
+        .findFirst()
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found"));
+    return ResponseEntity.ok(stud);
+  }
 
   @GetMapping("students")
-  public List<Student> getStudents(){
+  public List<Student> getStudents() {
     return students;
   }
 
-  //   private List<Student> students = Arrays.asList(
-  //     new Student(1, "Glauco", "Todesco", "13311-215", "Centro", "Sorocaba", "SP")
+  // private List<Student> students = Arrays.asList(
+  // new Student(1, "Glauco", "Todesco", "13311-215", "Centro", "Sorocaba", "SP")
   // );
 
 }
